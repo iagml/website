3379383dc919b693d25267fdc75ccec949e4ee72
Natanael <natanael_magalhaes@msn.com>
2020-03-31 10:59:43 -0300

    inline pdf filter

---
+++ _plugins/inline_pdf.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  module InlinePDF
+    def pdf(input)
+      path = "assets/uploads/#{input}"
+      if File.exist?(path)
+        output = "<iframe src='/#{path}' width='100%' height='600px'"
+        output << " frameborder='0' class='my-2' "
+        output << ' style="border: none;">'
+        output << "</iframe>"
+      else
+        output = "Arquivo não encontrado: #{input}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::InlinePDF)
