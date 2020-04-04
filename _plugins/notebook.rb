31fcd429a0c02c1050a6b1eeb6e3feb40b3b9636
Natanael <natanael_magalhaes@msn.com>
2020-04-04 09:12:02 -0300

    notebook plugin: add "open in colabs" button

--- _plugins/notebook.rb
+++ _plugins/notebook.rb
@@ -4,10 +4,15 @@ module Jekyll
       path = "notebooks/#{input}.html"
 
       if File.exist?(path)
-        output = '<iframe src="/notebooks/' + input + '.html" '
+        output = '<div>'
+        output << "<a href=\"https://colab.research.google.com/github/iagml/iagml.github.io/blob/master/assets/uploads/#{input}.ipynb\">"
+        output << '<img class="mt-5" src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab" />'
+        output << '</a>'
+        output << "<iframe src=\"/notebooks/#{input}.html\" "
         output << ' frameborder="0" scrolling="no" '
         output << ' onload="this.height=this.contentWindow.document.body.scrollHeight;" '
         output << ' style="width: 100%; border:none; overflow: auto;"></iframe>'
+        output << '</div>'
         output
       else
         "<pre><code>Arquivo: \"#{input}.ipynb\" não encontrado</code></pre>"
